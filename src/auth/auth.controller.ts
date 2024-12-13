f0004ec393c6e00197d3190da723f7b3e1d6d134
Makenna Worley <mworley@kengarff.com>
2024-12-13 12:07:31 -0700

    1:17:27

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, ParseEnumPipe, ParseIntPipe, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { AuthDto } from './dto';
 
 @Controller({
     path: "auth"
@@ -8,7 +9,7 @@ export class AuthController{
     constructor(private authService: AuthService) {}
 
     @Post('signup')
-    signup(@Body() dto: any) {
+    signup(@Body() dto: AuthDto) {
         return this.authService.signup(dto);
     }
 
--- src/auth/dto/index.ts
+++ src/auth/dto/index.ts
@@ -1,1 +1,1 @@
-export * from './autho.dto';
+export * from './auth.dto';
